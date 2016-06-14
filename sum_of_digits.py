fbfa59f1966638f7b1482ce893a860920ffd3a0a
Rishbh Sharma <rishbhsharma2008@gmail.com>
2016-06-14 17:45:22 +0530

    Sum of digits
    Calculates the sum of individual digits in a number

---
+++ sum_of_digits.py
@@ -0,0 +1,10 @@
+#returns the sum of digits
+#sod
+#example sod(12345)=1+2+3+4+5=15 ; sod(1)=1 ; sod(5)=1 ; sod(12)=1+2
+#the num should be of type of string
+
+def sod(num):
+  sum=0
+  for digit in num:
+    sum+=int(digit)
+  return int(sum)
